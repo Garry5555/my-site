91c396cf935d56c479d9297f813ec6331c6cc3ae
Garry5555 <132694027+Garry5555@users.noreply.github.com>
2023-05-07 08:53:49 +0000

    bg black

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
-import Image from 'next/image'
-import img from '../styles/result.jpg'
-
 function Home() {
   return (
     <main className="main">
-      <Image src={img} width={300} height={600}></Image>
+      <img src="/result.jpg" ></img>
     </main>
   )
 }
